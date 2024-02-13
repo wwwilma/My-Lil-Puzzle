afc4c5f394e40c87390dc62d18a089ad24309ce5
wnordlund <wilma.nordlund@gmail.com>
2024-02-13 22:23:48 +0100

    Create helpers.tsx with placeholder helpers

---
+++ src/helpers/helpers.tsx
@@ -0,0 +1,8 @@
+// Placeholders for future helpers
+export function formatNumber(number: number): string {
+    return number.toString();
+  }
+  
+  export function capitalizeFirstLetter(str: string): string {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
