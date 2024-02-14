86427cc34f808654f56c23e3a20e0db735f4e83a
wnordlund <wilma.nordlund@gmail.com>
2024-02-14 12:51:13 +0100

    Tile component and comments

--- src/components/tile/Tile.tsx
+++ src/components/tile/Tile.tsx
@@ -2,13 +2,47 @@ import React from 'react';
 import "./Tile.css"
 
 type TileProps = {
-    isCorrect: boolean;
+    isCorrect: boolean; 
     isHidden: number;
-    number: number;
-    size: number;
-    tileId: number;
+    number: number; 
     onClick: () => void;
-    cols: number;
+    tileId: number; 
+    size: number; 
+    cols: number; 
 };
 
-export {};
+/**
+ * Tile component representing a tile in the puzzle.
+ * @param isCorrect Tells if the tile is in the correct position.
+ * @param isHidden Tells if the tile is hidden, i.e. 0 for visible and 1 for hidden
+ * @param number Number displayed on tile
+ * @param onClick Callback function to handle tile click
+ * @param tileId  Identifier of the tile
+ * @param size Size of the tile
+ * @param cols The number of columns
+ * @returns 
+ */
+const Tile: React.FC<TileProps> = ({
+    isCorrect,
+    isHidden,
+    number,
+    onClick,
+    tileId,
+    size,
+    cols,
+}) => {
+    // Dynamically generate classes for the tile based on its properties
+    const tileClasses = `tile ${isCorrect ? "correct" : ""} ${isHidden ? "hidden" : ""}`;
+
+    return (
+        <div
+        className={tileClasses}
+        onClick={onClick}
+        style={{ "--tile-size": `${size}px`, "--cols": cols } as React.CSSProperties}
+        >
+            <span className="tile-number">{number}</span>    
+        </div>
+    );
+};
+
+export default Tile;
