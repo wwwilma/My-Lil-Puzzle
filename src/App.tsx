4656186ac2b79254e463900d92bca87aa9e742bc
wnordlund <wilma.nordlund@gmail.com>
2024-02-14 14:30:07 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import Game from "./components/game/Game";
 import ".App.css";
 
-export {};
+function App() {
+  const rows = 4;
+  const cols = 4;
+
+  return (
+    <div className="App">
+      <Game rows={rows} cols={cols}/>
+    </div>
+  );
+}
+
+export default App;
