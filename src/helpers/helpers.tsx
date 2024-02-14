c7d9929a182195c5436d6e730b21d49365f6f6ab
wnordlund <wilma.nordlund@gmail.com>
2024-02-14 11:33:24 +0100

    Update helpers with getBoardSize, generateArray

--- src/helpers/helpers.tsx
+++ src/helpers/helpers.tsx
@@ -6,3 +6,13 @@ export function formatNumber(number: number): string {
   export function capitalizeFirstLetter(str: string): string {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
+export const getBoardSize = (cols: number, rows: number): number => cols * rows;
+
+export const generateArray = (cols: number, rows: number): number[] => {
+    const size = getBoardSize(cols, rows);
+    const array: number [] = [];
+    for (let i = 0; i < size; i++) {
+        array.push(i + 1);
+    }
+    return array;
+};
