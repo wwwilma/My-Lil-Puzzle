ed51f422105cac06c7d18cfada58b80aee0dafd5
wnordlund <wilma.nordlund@gmail.com>
2024-02-14 09:26:39 +0100

    Button onClick

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,4 +1,17 @@
 import React from 'react';
-import "./Button.css"
+import "./Button.css";
 
-export{};
+type ButtonProps = {
+    text: string;
+    onClick?: () => void;
+};
+
+const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+    return (
+        <button className="button" onClick = {onClick}>
+            {text}
+        </button>
+    );
+};
+
+export default Button;
